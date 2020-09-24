e6b2b81e4abd49c736354fa1a2f6ad2a54575193
shuntatakemoto <rt80405@gmail.com>
2020-09-24 16:51:26 +0900

    Initial Commit

---
+++ HARUTA'sToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HARUTA'sToDoApp
+//
+//  Created by shunta takemoto on 2020/09/24.
+//  Copyright © 2020 Shunta Takemoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
